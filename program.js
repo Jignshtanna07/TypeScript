5f0133335ba6383d4c8e2e5f0d62fe4787c6aef5
Jignesh Tanna <jtanna@asite.com>
2023-10-30 15:59:54 +0530

    variavble and datatypes

---
+++ program.js
@@ -0,0 +1,12 @@
+// Datatypes
+var user = {
+    name: "Jignesh Tanna",
+    age: 22
+};
+console.log(user.name);
+//  available types in typeScript
+var geretting = "Darshan Joshi"; //it gives only  string methods 
+console.log(geretting);
+var gerettings = true;
+var UserId = 12345678;
+UserId.toFixed(); //it gives only number methods 
--- program.ts
+++ program.ts
@@ -1,1 +1,16 @@
 // Datatypes
+const user = {
+    name:"Jignesh Tanna",
+    age:22
+}
+console.log(user.name)
+//  available types in typeScript
+
+let geretting :string = "Darshan Joshi" //it gives only  string methods 
+console.log(geretting)
+
+let gerettings:boolean=true
+
+
+let UserId:Number=12345678
+UserId.toFixed()  //it gives only number methods 
