ca8f02ab00196d407e6d22dbc974ab706c4d6bc6
Jignesh Tanna <jtanna@asite.com>
2023-10-30 18:07:03 +0530

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+function sum(num) {
+    console.log(num + 3);
+}
+// console.log(sum(7))
+sum(7);
+function getuserdetails(name, email, ispaid) {
+    console.log(name + " you have " + email + " from " + ispaid + " correct");
+}
+getuserdetails("jignesh", "jignesh@gmail.com", false);
---
+++ function.ts
@@ -0,0 +1,15 @@
+function sum(num:number){
+    console.log(num+3)
+}
+// console.log(sum(7))
+sum(7)
+
+function getuserdetails(name:string,email:string,ispaid:boolean){
+    console.log(name + " you have " + email + " from " + ispaid + " correct")
+}
+getuserdetails("jignesh","jignesh@gmail.com",false)
+
+const  getsamsh = (name:string,email:string): string=>{
+    return " "
+}
+export{} 
