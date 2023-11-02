f50781f5d625eb14a4faa16a4e19adcef3049f30
Jignesh Tanna <jtanna@asite.com>
2023-11-02 11:29:23 +0530

    changes done in uniontype

--- uniontype.ts
+++ uniontype.ts
@@ -3,3 +3,23 @@ let score : number | string = "jignesh"
 console.log(score)
 score =5;
 console.log(score)
+
+type User = {
+    name :string,
+    isActive :boolean
+}
+
+
+type Admin = {
+    name :string,
+    id:number
+}
+ 
+let userOne : User | Admin = {
+    name :"Jignesh Tanna",
+    id:53
+}
+//   for example of pi this is new in typescript
+    let pi:3.14 = 3.14
+    pi=145
+export {}
